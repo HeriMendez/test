8ed000fc1e9afb5bddcfbd5e0ef7c389879f44a3
Heriberto Mendez <heri.mende@gmail.com>
2025-11-19 14:45:44 -0600

    Primer commit: Proyecto Java inicial

---
+++ src/main/java/mx/com/test/EnvioMailApplication.java
@@ -0,0 +1,13 @@
+package mx.com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/test/EnvioMailApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
